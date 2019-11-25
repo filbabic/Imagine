d88c35f4b492b0b8aaca8d5fe3cb190966f4ee68
Filip Babic <fbabic96@gmail.com>
2019-11-25 17:39:30 +0100

    Initial commit

---
+++ app/src/main/java/com/kolo/gorskih/tica/imagine/App.kt
@@ -0,0 +1,14 @@
+package com.kolo.gorskih.tica.imagine
+
+import android.app.Application
+
+/**
+ * Creates everything needed for DI and the project to work.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // setup
+    }
+}
---
+++ app/src/main/java/com/kolo/gorskih/tica/imagine/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kolo.gorskih.tica.imagine.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kolo.gorskih.tica.imagine.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
